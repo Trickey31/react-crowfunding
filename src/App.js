d39b366162f5a77c17a3bb6e2931f7fc5911ad89
Trickey <vngthnh999@gmail.com>
2022-08-18 13:02:48 +0700

    Update source code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+
+const SignUpPage = lazy(() => import("./pages/SignUpPage"));
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
