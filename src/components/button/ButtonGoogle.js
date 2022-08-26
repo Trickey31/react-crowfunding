e9a1cc81cf2090ae8ce2617c2198cd11d48c87c0
Trickey <vngthnh999@gmail.com>
2022-08-26 22:16:50 +0700

    Update ButtonGoogle component

--- src/components/button/ButtonGoogle.js
+++ src/components/button/ButtonGoogle.js
@@ -1,8 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withErrorBoundary } from "react-error-boundary";
+import ErrorComponent from "components/error/ErrorComponent";
 
-const ButtonGoogle = ({ text }) => {
+const ButtonGoogle = ({ text = "", onClick = () => {} }) => {
   return (
-    <button className="flex items-center justify-center gap-[10px] w-full border border-whiteStroke dark:border-darkStroke rounded-[10px] py-[13px] mb-5">
+    <button
+      className="flex items-center justify-center gap-[10px] w-full border border-whiteStroke dark:border-darkStroke rounded-[10px] py-[13px] mb-5"
+      onClick={onClick}
+    >
       <img srcSet="./google.png 2x" alt="icon-google" />
       <span className="font-semibold text-neutralText2 dark:text-white">
         {text}
@@ -11,4 +17,11 @@ const ButtonGoogle = ({ text }) => {
   );
 };
 
-export default ButtonGoogle;
+ButtonGoogle.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default withErrorBoundary(ButtonGoogle, {
+  FallbackComponent: <ErrorComponent></ErrorComponent>,
+});
