10f71c76bfbed5d2ecda101fa580309bf43a8ef4
Trickey <vngthnh999@gmail.com>
2022-08-28 21:50:11 +0700

    Update Topbar

---
+++ src/modules/dashboard/DashboardAvatar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DashboardAvatar = () => {
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1661577849879-470cbf27418f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+        alt=""
+        className="w-[52px] h-[52px] object-cover rounded-full"
+      />
+    </div>
+  );
+};
+
+export default DashboardAvatar;
--- src/modules/dashboard/DashboardTopbar.js
+++ src/modules/dashboard/DashboardTopbar.js
@@ -1,7 +1,30 @@
+import { Button } from "components/button";
 import React from "react";
+import DashboardAvatar from "./DashboardAvatar";
+import DashboardFund from "./DashboardFund";
+import DashboardSearch from "./DashboardSearch";
 
 const DashboardTopbar = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-[54px] flex-1 pl-3">
+        <img srcSet="./logo.png 2x" alt="logo" />
+        <div className="w-full max-w-[458px]">
+          <DashboardSearch></DashboardSearch>
+        </div>
+      </div>
+      <div className="flex items-center justify-end flex-1 gap-10">
+        <DashboardFund></DashboardFund>
+        <Button
+          type="button"
+          className="bg-secondary text-white font-semibold px-[26px]"
+        >
+          Start a campaign
+        </Button>
+        <DashboardAvatar></DashboardAvatar>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardTopbar;
