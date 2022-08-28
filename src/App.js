e87289c0123e53e593b9a17ba9b71d2a5d538e2f
Trickey <vngthnh999@gmail.com>
2022-08-28 07:41:17 +0700

    Initialize dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
-const SignUpPage = lazy(() => import("./pages/SignUpPage"));
-const SignInPage = lazy(() => import("./pages/SignInPage"));
+const SignUpPage = lazy(() => import("pages/SignUpPage"));
+const SignInPage = lazy(() => import("pages/SignInPage"));
+const LayoutDashboard = lazy(() => import("layout/LayoutDashboard"));
+const DashboardPage = lazy(() => import("pages/DashboardPage"));
 
 function App() {
   return (
     <Suspense>
       <Routes>
+        <Route element={<LayoutDashboard></LayoutDashboard>}>
+          <Route path="/" element={<DashboardPage></DashboardPage>}></Route>
+        </Route>
         <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
         <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
       </Routes>
