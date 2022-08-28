c3b4f17426757b4bc85f70885e65d7afc011a16b
Trickey <vngthnh999@gmail.com>
2022-08-28 07:35:39 +0700

    Initialize dashboard

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,8 @@
+import LayoutDashboard from "layout/LayoutDashboard";
+import React from "react";
+
+const DashboardPage = () => {
+  return <LayoutDashboard></LayoutDashboard>;
+};
+
+export default DashboardPage;
